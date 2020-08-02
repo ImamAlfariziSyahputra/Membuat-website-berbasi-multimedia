ee8fc9ca4364e726b9c3f5941badcd0adbf0e647
Imam Alfarizi Syahputra <63343105+ImamAlfariziSyahputra@users.noreply.github.com>
2020-08-02 11:26:12 +0700

    Membuat website berbasis multimedia
    - Menggunakan Framework Bootsrap
    - Menampilkan Video dan Audio

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <style>
+        
+        
+    </style>
+    <title>Multimedia</title>
+</head>
+<body id="top">
+    
+<nav class="navbar navbar-expand-lg navbar-light fixed-top bg-transparent">
+    <div class="container">
+        <a class="navbar-brand text-white p-2" href="#top">LAZY LEARNING WEBSITE</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+            <li class="nav-item active mr-3">
+                <a class="nav-link js-scroll-trigger" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item mr-3">
+                <a class="nav-link js-scroll-trigger" href="#video">Video</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#music">Chill Music</a>
+            </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="jumbotron text-white text-center">
+    
+    <p class="display-4">EXPLORE YOUR SELF WITH <br> <span class="font-weight-bold">LAZY LEARNING</span></p>
+    <hr class="my-4">
+    <p class="lead"><u>Learning anything about Website Porgramming, and Chilling Music.</u></p>
+</div>
+<!-- video -->
+<div class="video p-5 bg-dark" id="video">
+    <h1 class="text-white text-center mb-3">VIDEO LIST</h1>
+    <div class="row no-gutters mb-3">
+        <div class="col-4 text-center">
+            <video width="320" height="200" class="" controls autoplay poster="img/thumbnail.jpg">
+                <source src="file/video/oop1.mp4" type="video/mp4">
+            </video>
+        </div>
+        <div class="col-4 text-center">
+            <video width="320" height="200" class="" controls poster="img/thumbnail.jpg">
+                <source src="file/video/oop2.mp4" type="video/mp4">
+            </video>
+        </div>
+        <div class="col-4 text-center">
+            <video width="320" height="200" class="" controls poster="img/thumbnail.jpg">
+                <source src="file/video/oop3.mp4" type="video/mp4">
+            </video>
+        </div>
+    </div>
+    <div class="row no-gutters">
+        <div class="col-4 text-center">
+            <video width="320" height="200" class="" controls poster="img/thumbnail.jpg">
+                <source src="file/video/oop1.mp4" type="video/mp4">
+            </video>
+        </div>
+        <div class="col-4 text-center">
+            <video width="320" height="200" class="" controls poster="img/thumbnail.jpg">
+                <source src="file/video/oop2.mp4" type="video/mp4">
+            </video>
+        </div>
+        <div class="col-4 text-center">
+            <video width="320" height="200" class="" controls poster="img/thumbnail.jpg">
+                <source src="file/video/oop3.mp4" type="video/mp4">
+            </video>
+        </div>
+    </div>
+</div>
+<!-- audio -->
+<div class="music bg-primary p-5" id="music">
+    <h1 class="text-white text-center mb-5">MUSIC LIST</h1>
+    <div class="row no-gutters mb-5">
+        <div class="col-4 text-center pt-1">
+            <p class="text-white">James Arthur - Say you won't let go</p>
+            <audio src="file/music/song.mp3" controls autoplay></audio>
+        </div>
+        <div class="col-4 text-center pt-1">
+            <p class="text-white">James Arthur - Say you won't let go</p>
+            <audio src="file/music/song.mp3" controls></audio>
+        </div>
+        <div class="col-4 text-center pt-1">
+            <p class="text-white">James Arthur - Say you won't let go</p>
+            <audio src="file/music/song.mp3" controls></audio>
+        </div>
+    </div>
+    <div class="row no-gutters mb-5">
+        <div class="col-4 text-center pt-1">
+            <p class="text-white">James Arthur - Say you won't let go</p>
+            <audio src="file/music/song.mp3" controls></audio>
+        </div>
+        <div class="col-4 text-center pt-1">
+            <p class="text-white">James Arthur - Say you won't let go</p>
+            <audio src="file/music/song.mp3" controls></audio>
+        </div>
+        <div class="col-4 text-center pt-1">
+            <p class="text-white">James Arthur - Say you won't let go</p>
+            <audio src="file/music/song.mp3" controls></audio>
+        </div>
+    </div>
+    <div class="row no-gutters">
+        <div class="col-4 text-center pt-1">
+            <p class="text-white">James Arthur - Say you won't let go</p>
+            <audio src="file/music/song.mp3" controls></audio>
+        </div>
+        <div class="col-4 text-center pt-1">
+            <p class="text-white">James Arthur - Say you won't let go</p>
+            <audio src="file/music/song.mp3" controls></audio>
+        </div>
+        <div class="col-4 text-center pt-1">
+            <p class="text-white">James Arthur - Say you won't let go</p>
+            <audio src="file/music/song.mp3" controls></audio>
+        </div>
+    </div>
+</div>
+
+
+
+    
+
+    
+    
+</body>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</html>
